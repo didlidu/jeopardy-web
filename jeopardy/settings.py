3e99015eace16e6b081adba0edaa8ff80f26ebd0
Artem Samokhin <bunjdo@gmail.com>
2019-08-07 22:04:40 +1000

    commit

--- jeopardy/settings.py
+++ jeopardy/settings.py
@@ -58,13 +58,16 @@ WSGI_APPLICATION = 'jeopardy.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+# }
 
+DATABASES = {
+    'default': django_heroku.dj_database_url.config()
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
